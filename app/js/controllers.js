6ae64a9c1f847db6a880c1767ef57adfe5da88cd
Siorski <kacpersiora@wp.pl>
2014-06-02 14:30:36 +0200

    clean project

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,4 @@
-'use strict';
+var myApp = angular.module('myApp.controllers', []);
 
-/* Controllers */
-
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+myApp.controller('KursyController', [ function () {
+}]);
